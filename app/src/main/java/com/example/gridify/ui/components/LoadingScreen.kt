0ae2654e7284417b7fe8658fb0d2754c4dcfa13e
Kavearhasi <kavearhasi28@gmail.com>
2026-01-20 21:05:47 +0530

    create the loading  screen

--- app/src/main/java/com/example/gridify/ui/components/LoadingScreen.kt
+++ app/src/main/java/com/example/gridify/ui/components/LoadingScreen.kt
@@ -1,11 +1,24 @@
 package com.example.gridify.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.gridify.R
 
 @Composable
 fun LoadingScreen() {
@@ -14,5 +27,29 @@ fun LoadingScreen() {
         modifier = Modifier
             .fillMaxHeight(.8f)
             .fillMaxWidth()
-    ) { }
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.gridify_logo),
+                modifier = Modifier.size(128.dp),
+                contentDescription = "The app logo"
+            )
+            LinearProgressIndicator(
+                color = MaterialTheme.colorScheme.onPrimary,
+                modifier = Modifier
+                    .width(128.dp)
+                    .padding(16.dp)
+            )
+            Text(
+                text = "Loading...",
+                style = MaterialTheme.typography.labelLarge,
+                modifier = Modifier.wrapContentSize()
+            )
+
+        }
+
+    }
 }
